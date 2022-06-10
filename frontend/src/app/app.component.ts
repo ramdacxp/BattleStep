841d1ff8ff6bc8152352bdc72ecb47c346e975f9
Michael Hoser <michael.hoser@outlook.com>
2022-06-10 22:55:02 +0200

    Routing with #

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Page } from './models/page';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: []
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'BattleStep';
+  pages: Page[] = [
+    { name: 'home', label: 'Willkommen' },
+    { name: 'now', label: 'Heute' },
+    { name: 'history', label: 'Verlauf' }
+  ];
+
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,19 @@
+// Use Flowbite via WebPack
+import 'flowbite';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-// Use Flowbite via WebPack
-import 'flowbite';
-
+import { HomeViewComponent } from './views/home-view/home-view.component';
+import { NotFoundViewComponent } from './views/not-found-view/not-found-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent,
+    NotFoundViewComponent
   ],
   imports: [
     BrowserModule,
